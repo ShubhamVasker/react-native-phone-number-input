7601a3eaf9924cca46fc122127bbdbf9ac6414b9
FB0074 <getitrent@GETITRENTs-MacBook-Pro.local>
2024-01-18 18:28:02 +0530

    placeholderTextColor prop is added

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -30,6 +30,7 @@ export interface PhoneInputProps {
   layout?: "first" | "second";
   filterProps?: CountryFilterProps;
   countryPickerProps?: any;
+  placeholderTextColor?:StyleProp<TextStyle>
 }
 export interface PhoneInputState {
   code: CallingCode | undefined;
